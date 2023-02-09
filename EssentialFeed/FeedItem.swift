f4e06efa4078dc865c95f416c97bac06e8650ab0
vinod supnekar <vinod.supnekar@gmail.com>
2023-02-09 12:19:48 +0530

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Rjvi on 09/02/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
