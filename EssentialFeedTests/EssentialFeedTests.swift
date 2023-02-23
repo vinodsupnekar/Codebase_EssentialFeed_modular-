164f49b4a5a7552bd7a8621b95c3af989e021694
vinod supnekar <vinod.supnekar@gmail.com>
2023-02-23 14:13:03 +0530

    Add meomory leak delectation the logic in test

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -100,10 +100,20 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     //MARK:- Helpers
     
-    private func makeSUT(url: URL = URL(string: "https://a-given.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+    private func makeSUT(url: URL = URL(string: "https://a-given.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
+        
+        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(client)
+        
      return (sut, client)
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line :UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "instance should have been deallocated.Potential memory leak",file: file, line: line)
+        }
     }
     
     private func makeFeedItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
