5715c07db467baacb4b22c6c44ea80eaeda3a88c
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-10 17:31:08 +0530

    Save command does not requets cache insertion on deletion error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -16,16 +16,21 @@ class LocalFeedLoader {
         self.store = store
     }
     
-    func save(items: [FeedItem]) {
+    func save(_ items: [FeedItem]) {
         self.store.deleteCacheFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    var insertCallCount = 0
     
     func deleteCacheFeed() {
         self.deleteCachedFeedCallCount += 1
+    }
+    
+    func completeDeletion(with error: NSError, at index: Int = 0) {
+        
     }
 }
 
@@ -42,9 +47,20 @@ class CacheFeedUseCaseTests: XCTestCase {
         let (sut,store) = makeSUT()
         let items = [uniqueItem(),uniqueItem()]
         
-        sut.save(items: items)
+        sut.save(items)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
     }
     
     // MARK:- helpers
@@ -55,6 +71,10 @@ class CacheFeedUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
     
     private func uniqueItem() -> FeedItem {
