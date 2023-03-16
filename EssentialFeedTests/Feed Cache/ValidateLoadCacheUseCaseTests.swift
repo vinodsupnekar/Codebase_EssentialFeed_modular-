2629eaaeee84cec5aa84a8a4a35520ed26de793b
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-16 06:55:05 +0530

    Local feed loader does not request feed upon creation

---
+++ EssentialFeedTests/Feed Cache/ValidateLoadCacheUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  ValidateLoadCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Rjvi on 16/03/23.
+//
+
+import XCTest
+import EssentialFeed
+
+class ValidateLoadCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK:- helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
