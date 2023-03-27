812b8d5782b2217a58251163b4171f83b562a8fe
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-27 17:33:48 +0530

    Retrieving after inserting cache to empty cache delivers inserted values with setUp and tearDown block , Move Codable conformance from the Framework agnostic "LocalFeedImage" type to the new framework specific "CodableFeedImage" type. The CodableFeedImage is a private type within the framework implementation since the coddle requirement is framework specific requirement

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,12 +9,71 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+         let feed: [CodableFeedImage]
+         let timestamp: Date
+        
+        var localFeed: [LocalFeedImage] {
+            return feed.map { $0.local }
+        }
+    }
+    
+    private struct CodableFeedImage: Equatable, Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        init(_ image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            return LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    
     func retrieve(_ completion: @escaping FeedStore.RetrievalCompletion ) {
-        completion(.emtpy)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.emtpy)
+        }
+        
+        let decoder = JSONDecoder()
+        let cache = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        
+    }
+    
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let cache = Cache(feed: feed.map { CodableFeedImage($0)}, timestamp: timestamp)
+        let encoded = try! encoder.encode(cache)
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
 class CodableFeedStoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+
+    }
     
     func test_retrieve_deleiversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
@@ -32,6 +91,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyache() {
+        
         let sut = CodableFeedStore()
         let exp = expectation(description: "wait for cache retrieval")
         sut.retrieve { firstResult in
@@ -41,6 +101,30 @@ class CodableFeedStoreTests: XCTestCase {
                 break
             default:
                 XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult)  and \(secondResult) instead")
+            }
+            exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieve_AfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let exp = expectation(description: "wait for cache retrieval")
+        
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            sut.retrieve { retrivedResult in
+            switch retrivedResult {
+            case let .found(feed: retrivedResult, timestamp: retrivedTimestamp):
+                XCTAssertEqual(retrivedResult, feed)
+                XCTAssertEqual(retrivedTimestamp, timestamp)
+
+                break
+            default:
+                XCTFail("Expected found result with feed \(feed) and timestamp \(timestamp), got \(retrivedResult) instead")
             }
             exp.fulfill()
             }
