3adaf97e972a5208bc5b075339cd105dff6b744f
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-28 07:57:13 +0530

    Retrieve delivers failure on retrieval error

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -47,10 +47,13 @@ class CodableFeedStore {
             return completion(.emtpy)
         }
         
-        let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found((feed: cache.localFeed, timestamp: cache.timestamp)))
-        
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found((feed: cache.localFeed, timestamp: cache.timestamp)))
+        } catch {
+            completion(.failure(error))
+        }
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -110,6 +113,14 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found((feed, timestamp)))
     }
     
+    func test_retrieve_deliversFailureOnRetrievalError() {
+        let sut = makeSUT()
+        
+        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
+    
     // -MARK:- Helpers
     
     private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date) ,to sut: CodableFeedStore) {
@@ -132,7 +143,8 @@ class CodableFeedStoreTests: XCTestCase {
 
         sut.retrieve { receivedResult in
             switch (receivedResult, expectedResult) {
-            case (.emtpy, .emtpy) :
+            case (.emtpy, .emtpy),
+                 (.failure, .failure):
                 break
                 
             case let (.found(expectedResult), .found(retrieved)):
