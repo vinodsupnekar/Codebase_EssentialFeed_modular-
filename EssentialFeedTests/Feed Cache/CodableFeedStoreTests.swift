20d24cda0bc2b99fd5e25269cac95dc606bb526c
vinod supnekar <vinod.supnekar@gmail.com>
2023-03-30 07:56:41 +0530

    Extract reusable "FeedStore" helper methods into a shared scope  as it can be used by other "FeedStore" implementation tests

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -211,56 +211,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStore  {
     }
     
     // -MARK:- Helpers
-    @discardableResult
-    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date) ,to sut: FeedStore) -> Error? {
-        var insertionError: Error?
-        let exp = expectation(description: "wait for cache retrieval")
-        sut.insert(cache.feed, timeStamp: cache.timestamp) { receivedInsertionError in
-            insertionError = receivedInsertionError
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-        return insertionError
-    }
-    
-    @discardableResult
-    private func deleteCache(from sut: FeedStore) -> Error? {
-        var deletionError: Error?
-        let exp = expectation(description: "wait for cache deletion")
-        sut.deleteCacheFeed { receivedDeletionError in
-            deletionError = receivedDeletionError
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-        return deletionError
-    }
-    
-    private func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
-        
-        expect(sut, toRetrieve: expectedResult, file: file, line: line)
-        expect(sut, toRetrieve: expectedResult, file: file, line: line)
-    }
-    
-    private func expect(_ sut: FeedStore, toRetrieve expectedResult: RetrieveCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
-        let exp = expectation(description: "wait for load completion")
 
-        sut.retrieve { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case (.emtpy, .emtpy),
-                 (.failure, .failure):
-                break
-                
-            case let (.found(expectedResult), .found(retrieved)):
-                XCTAssertEqual(expectedResult.feed, retrieved.feed, file: file, line: line)
-                XCTAssertEqual(expectedResult.timestamp, retrieved.timestamp, file: file, line: line)
-
-            default:
-                XCTFail("Expected to retrieve \(expectedResult), got \(receivedResult) instead")
-            }
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
-    }
     
     private func makeSUT( storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> FeedStore {
         let sut = CodableFeedStore(storeURL: storeURL ?? testSpecificStoreURL())
