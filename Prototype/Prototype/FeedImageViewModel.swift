e7b60e89de34602c92cc765e38bdfe71c6af8140
vinod supnekar <vinod.supnekar@gmail.com>
2023-04-02 12:02:06 +0530

    Add prototype feed data for demoing purposes

---
+++ Prototype/Prototype/FeedImageViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageViewModel.swift
+//  Prototype
+//
+//  Created by Rjvi on 02/04/23.
+//
+
+import Foundation
+
+extension FeedImageViewModel {
+    static var prototype: [FeedImageViewModel] {
+        return [FeedImageViewModel(description: "asasas asas asas asas as asasas asasssssas asasasdferer dfcv  cvcv ", location: "ASas,Mild", imageName: "image-0"),
+                FeedImageViewModel(description: "qwqw dfdf asasas asas asas asas as asasas asasssssas asasasdferer dfcv  cvcv ffp ereryhgn ", location: nil, imageName: "image-1"),
+                FeedImageViewModel(description: nil, location: "Cannot Pallace,New Delhi", imageName: "image-2")]
+    }
+}
