56b8dd36e737525a11d41bd678fe1d9d845fd2e8
vinod supnekar <vinod.supnekar@gmail.com>
2023-04-06 19:48:55 +0530

    Add UIRefreshControl to prototype to simulate async loading of the feed

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -15,8 +15,26 @@ struct FeedImageViewModel {
 }
 
 class FeedViewController: UITableViewController {
+        
+    private var feed = [FeedImageViewModel]()
     
-    private let feed = FeedImageViewModel.prototype
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        refresh()
+        tableView.setContentOffset(CGPoint(x: 0, y: -tableView.contentInset.top), animated: true)
+    }
+    
+    @IBAction func refresh() {
+        refreshControl?.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            if self.feed.isEmpty {
+                self.feed = FeedImageViewModel.prototype
+                self.tableView.reloadData()
+            }
+            self.refreshControl?.endRefreshing()
+        }
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return feed.count
