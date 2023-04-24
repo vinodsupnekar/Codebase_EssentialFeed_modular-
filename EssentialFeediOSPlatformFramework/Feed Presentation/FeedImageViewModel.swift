72465fe843cbf8e0f4866f29b286889fc6f14ea3
vinodsupnekar <vinodsupnekar@users.noreply.github.com>
2023-04-24 11:13:54 +0530

    Add FeedImagePresenter holding FeedImage presentation logic (including image loading state)

--- EssentialFeediOSPlatformFramework/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeediOSPlatformFramework/Feed Presentation/FeedImageViewModel.swift
@@ -9,57 +9,14 @@ import Foundation
 //import UIKit
 import EssentialFeed
 
-final class FeedImageViewModel<Image> {
-    typealias Observer<T> = (T) -> Void
-
-    private var task: FeedImageDataLoaderTask?
-    private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private let imageTranfrormer: (Data) -> Image?
-    
-    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTranfrormer: @escaping (Data) -> Image?) {
-        self.model = model
-        self.imageLoader = imageLoader
-        self.imageTranfrormer = imageTranfrormer
-    }
-    
-    var description: String? {
-            return model.description
-    }
-
-    var location: String?  {
-        return model.location
-    }
+struct FeedImageViewModel<Image> {
+    let description: String?
+    let location: String?
+    let image: Image?
+    let isLoading: Bool
+    let shouldRetry: Bool
 
     var hasLocation: Bool {
         return location != nil
     }
-    
-   
-
-    var onImageLoad: Observer<Image>?
-    var onImageLoadingStateChange: Observer<Bool>?
-    var onShouldRetryImageLoadStateChange: Observer<Bool>?
-    
-    func loadImageData() {
-        onImageLoadingStateChange?(true)
-        onShouldRetryImageLoadStateChange?(false)
-        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            self?.handle(result)
-        }
-    }
-    
-    private func handle(_ result: FeedImageDataLoader.Result) {
-        if let image = (try? result.get()).flatMap(imageTranfrormer) {
-            onImageLoad?(image)
-        } else {
-            onShouldRetryImageLoadStateChange?(true)
-        }
-        onImageLoadingStateChange?(false)
-    }
-    
-    func cancelImageDataLoad() {
-            task?.cancel()
-            task = nil
-        }
 }
