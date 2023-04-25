cd455796f1d97787ff8e3c706f627ba087d5bb5a
vinodsupnekar <vinodsupnekar@users.noreply.github.com>
2023-04-25 10:17:50 +0530

    Extract system under test creation to a test factory method

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -16,14 +16,26 @@ final class FeedPresenter {
 class FeedPresenterTests: XCTestCase {
 
     func test_init_doesNotSendMessagesToView() {
-        let view = ViewSpy()
+        let (_,view) = makeSUT()
         
         _ = FeedPresenter(view: view)
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
-    private class ViewSpy {
-    let messages = [Any]()
+    // MARK:- Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
     }
+    
+    private class ViewSpy {
+        let messages = [Any]()
+    }
+    
 }
